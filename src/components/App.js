695589b7fc61d4421b0d6438339276b88eb992ba
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-17 12:28:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+ <Welcome name="pallavi " />
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
+      <input id='input' onChange={pallavi}></input>
       <button id='button' onClick={}>Click</button>
       <p id='text'> Hello my name is ____ and I study at Newton School</p>
     </div>
